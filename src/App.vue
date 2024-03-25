6a0f69479d0b1a9543ada19f783dd165bf21aa6e
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-03-25 22:11:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script>
+import { store } from "./data/store.js";
+import axios from "axios";
+import Header from "./components/Header.vue";
+import Main from "src/assets/mixin.scss";
+export default {
+  components: {
+    Header,
+    Main,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getApi() {
+      axios
+        .get(this.store.apiUrl)
+        .then((res) => {
+          console.log("nella function", res.data.results);
+          this.store.cardList = res.data.results;
+          console.log("nello store", this.store.cardList[0]);
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+
+  mounted() {
+    this.getApi();
+  },
+};
+</script>
+
+<template>
+  <div class="wrapper">
+    <Header /><br />
+    <Main />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "./assets/scss/main.scss";
+
+.wrapper {
+  // background: url(https://images.unsplash.com/photo-1518141532615-4305c9f914c9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODJ8fE5lcm98ZW58MHx8MHx8fDA%3D)
+  //   top / cover no-repeat fixed/ rotateY(90deg);
+  background-image: linear-gradient(to right, black, rgb(72, 143, 0), black);
+}
+</style>
+  <Header /><br />
+  <Main />
+</template>
+
+<style lang="scss" scoped>
+@use "./assets/scss/main.scss";
+</style>./components/Main,vue/index.js
---
+++ src/components/partials/Card.vue
@@ -0,0 +1,166 @@
+<script>
+export default {
+  props: {
+    cardName: String,
+    cardStatus: String,
+    cardGender: String,
+    cardSpecies: String,
+    cardImage: String,
+    cardLocation: String,
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="imgBx">
+        <img :src="cardImage" :alt="cardName" />
+      </div>
+      <div class="name">
+        <h2>{{ cardName }}</h2>
+        <div class="status">
+          <h3>{{ cardStatus }}</h3>
+        </div>
+        <div class="gender">
+          <h3>{{ cardGender }}</h3>
+        </div>
+        <div class="gender">
+          <h3>{{ cardLocation }}</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  position: relative;
+
+  .card {
+    position: relative;
+    width: 350px;
+    height: 450px;
+    background: #232323;
+    border-radius: 20px;
+    overflow: hidden;
+    box-shadow: 1px 1px 10px 13px rgb(255, 255, 255);
+
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: #d5d5d5;
+      clip-path: circle(450px at 50% 80%);
+      transition: 0.3s ease;
+    }
+
+    &:hover:before {
+      clip-path: circle(100px at 50% -20%);
+    }
+
+    &:after {
+      content: url("https://pendragongamestudio.com/it/wp-content/uploads/sites/2/2019/05/0wzY6AgU.png");
+      position: absolute;
+      top: 30%;
+      left: -40%;
+      transform: scale(0.4);
+    }
+    &hover:after {
+      transform: translateY(20px);
+    }
+
+    .imgBx {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      z-index: 9999;
+      width: 100%;
+      height: 220px;
+      transition: 0.3s;
+      border-radius: 10px;
+
+      img {
+        position: absolute;
+        top: 55%;
+        left: 50%;
+        transform: translate(-50%, -50%) rotate(-0deg);
+        width: 280px;
+        border-radius: 10px;
+      }
+    }
+
+    .name {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      height: 350px;
+      text-align: center;
+      transition: 0.6s;
+      z-index: 10;
+
+      h2 {
+        position: relative;
+        font-weight: 600;
+        font-size: 2rem;
+        color: #ffffff;
+        margin-bottom: 10px;
+      }
+
+      .status,
+      .gender {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 8px 20px;
+        transition: 0.5s;
+        opacity: 0;
+        visibility: hidden;
+        padding-top: 0;
+        padding-bottom: 0;
+
+        h3 {
+          color: #b0b0b0;
+          font-weight: 600;
+          font-size: 18px;
+          text-transform: uppercase;
+
+          margin-right: 10px;
+        }
+      }
+
+      .gender span {
+        width: 20px;
+        height: 20px;
+        background: rgb(255, 255, 255);
+        border-radius: 50%;
+        margin: 0 5px;
+      }
+    }
+
+    &:hover .imgBx {
+      top: 1%;
+      transform: translateY(0%);
+      transform: scale(90%);
+    }
+
+    &:hover .name {
+      height: 180px;
+      .status {
+        opacity: 1;
+        visibility: visible;
+        transition-delay: 0.3s;
+      }
+
+      .gender {
+        opacity: 1;
+        visibility: visible;
+        transition-delay: 0.6s;
+      }
+    }
+  }
+}
+</style>./card.vue
